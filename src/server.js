c528fd3898358df376840ec655e52eeae65497ec
Carlos Simon <casimon@redhat.com>
2021-05-03 13:36:43 -0500

    missing pipe

--- src/server.js
+++ src/server.js
@@ -5,7 +5,7 @@ const app = express()
 const port = 3000
 
 
-app.get('/', (_, res) => res.send(process.env.MESSAGE | 'Test'))
+app.get('/', (_, res) => res.send(process.env.MESSAGE || 'Test'))
 
 app.listen(port, () => console.log("Server's up"))
 
